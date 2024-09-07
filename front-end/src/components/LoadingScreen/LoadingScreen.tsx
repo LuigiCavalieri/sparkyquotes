45369e354ea97eb4a1c44cdb95fd5b3d4907f85b
Luigi Cavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2024-09-07 12:54:07 +0200

    Added a delay on showing the loading screen.

--- front-end/src/components/LoadingScreen/LoadingScreen.tsx
+++ front-end/src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function LoadingScreen() {
+	const [showScreen, setShowScreen] = useState(false);
+
+	useEffect(() => {
+		const timer = setTimeout(() => setShowScreen(true), 500);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, []);
+
+	if (!showScreen) {
+		return null;
+	}
+
 	return (
 		<div data-testid="loading-screen" className="h-full flex items-center justify-center">
 			Loading...
